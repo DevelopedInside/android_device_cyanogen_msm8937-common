cf3464f1a28946bc5b55abeb91bfaba71a0c92e3
BeYkeRYkt <beykerykt@gmail.com>
2017-05-29 04:07:04 +0900

    msm8937: update wifi

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -1,15 +1,18 @@
 # WCNSS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_PACKAGES += \
-    wcnss_service	
+    wcnss_service
 	
 # WiFi
 PRODUCT_PACKAGES += \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libwpa_client
+
+PRODUCT_PACKAGES += \
+    dhcpcd.conf
 
 PRODUCT_PACKAGES += \
     hostapd.accept \
