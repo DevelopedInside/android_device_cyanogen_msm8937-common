1d495ce8283ee1c8490df10c00c094e157c8ead3
BeYkeRYkt <beykerykt@gmail.com>
2017-06-12 04:41:02 +0900

    msm8937: move audio_effects to common
    * taked from CyanogenOS-13.1.5-marmite

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -5,3 +5,6 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf 
