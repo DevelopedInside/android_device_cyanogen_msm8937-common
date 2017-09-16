0dfc0750d3a5ce548729af6ec38e074784e9cfe0
BeYkeRYkt <beykerykt@gmail.com>
2017-09-16 21:45:23 +0900

    msm8937: fix audio config

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -7,4 +7,4 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/$(TARGET_BOARD_PLATFORM_VARIANT)/audio_effects.conf:system/vendor/etc/audio_effects.conf\
+    hardware/qcom/audio-caf/msm8996/configs/$(TARGET_BOARD_PLATFORM_VARIANT)/audio_effects.conf:system/vendor/etc/audio_effects.conf\
