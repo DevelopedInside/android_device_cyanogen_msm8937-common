921a5cad2a37aebc1773046e64d0675434f5f7af
BeYkeRYkt <beykerykt@gmail.com>
2017-09-18 21:46:12 +0700

    msm8937: format

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -6,5 +6,6 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing
 
+# Based on (https://github.com/LineageOS/android/commit/64e1481a189d34c2e9fa603571f1158164e3654d)
 PRODUCT_COPY_FILES += \
     hardware/qcom/audio-caf/msm8996/configs/$(TARGET_BOARD_PLATFORM_VARIANT)/audio_effects.conf:system/vendor/etc/audio_effects.conf\
